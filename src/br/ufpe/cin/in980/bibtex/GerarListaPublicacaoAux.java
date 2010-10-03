3ff2eb4168ee66e3d667e8d111c83b8bcd996a74
Juliana Lucena <julianalucenaa@gmail.com>
2010-10-03 15:28:41 -0300

    Refactor #6: Removendo duplicacoes de append na criacao do bibtex.

--- src/br/ufpe/cin/in980/bibtex/GerarListaPublicacaoAux.java
+++ src/br/ufpe/cin/in980/bibtex/GerarListaPublicacaoAux.java
@@ -11,99 +11,38 @@ import br.ufpe.cin.in980.publicacao.Publicacao;
 public class GerarListaPublicacaoAux {
 
 	public StringBuffer gerarPublicacaoBibTex(Publicacao publicacao, StringBuffer bib) {
+		
+		// Comum a todas as publicacoes
+		String nomesAutorTitulo = getNomes(publicacao) + ",\n author\t= \"" + getNomeAutores(publicacao) 
+									+ "\",\n title\t= \"" + publicacao.getTitulo() + "\",\n";
+		
 		if (publicacao instanceof ArtConferencia) {
+			
 			ArtConferencia conferencia = (ArtConferencia) publicacao;
-			bib.append("@Conference{");
-			bib.append(getNomes(conferencia));
-			bib.append(",\n");
-			bib.append("author\t= \"");
-			bib.append(getNomeAutores(conferencia));
-			bib.append("\",\n");
-			bib.append("title\t= \"");
-			bib.append(conferencia.getTitulo());
-			bib.append("\",\n");
-			bib.append("booktitle\t= \"");
-			bib.append("");
-			bib.append("\",\n");
-			bib.append("year\t= \"");
-			bib.append(conferencia.getAno());
-			bib.append("\",\n");
-			bib.append("pages\t= \"");
-			bib.append(conferencia.getPaginas());
-			bib.append("\",\n");
-			bib.append("month\t= \"");
-			bib.append(conferencia.getMes());
-			bib.append("\"\n}");
+			bib.append("@Conference{" + nomesAutorTitulo + "booktitle\t= \"" + "\",\n year\t= \"" + conferencia.getAno() + "\",\n pages\t= \"" + conferencia.getPaginas() 
+					+ "\",\n month\t= \"" + conferencia.getMes() + "\"\n}");
+
 		} else if (publicacao instanceof ArtPeriodicoRevista) {
+			
 			ArtPeriodicoRevista periodicoRevista = (ArtPeriodicoRevista) publicacao;
-			bib.append("@Article{");
-			bib.append(getNomes(periodicoRevista));
-			bib.append(",\n");
-			bib.append("author\t= \"");
-			bib.append(getNomeAutores(periodicoRevista));
-			bib.append("\",\n");
-			bib.append("title\t= \"");
-			bib.append(periodicoRevista.getTitulo());
-			bib.append("\",\n");
-			bib.append("journal\t= \"");
-			bib.append(periodicoRevista.getJournal());
-			bib.append("\",\n");
-			bib.append("year\t= \"");
-			bib.append(periodicoRevista.getAno());
-			bib.append("\",\n");
-			bib.append("volume\t= \"");
-			bib.append(periodicoRevista.getVolume());
-			bib.append("\",\n");
-			bib.append("number\t= \"");
-			bib.append(periodicoRevista.getNumero());
-			bib.append("\"\n}");
-			bib.append("pages\t= \"");
-			bib.append(periodicoRevista.getPaginas());
-			bib.append("\"\n}");
+			bib.append("@Article{" + nomesAutorTitulo + "journal\t= \"" + periodicoRevista.getJournal() + "\",\n year\t= \"" + periodicoRevista.getAno() 
+					+ "\",\n volume\t= \"" + periodicoRevista.getVolume() + "\",\n number\t= \"" + periodicoRevista.getNumero() 
+					+ "\"\n} pages\t= \"" + periodicoRevista.getPaginas() + "\"\n}");
+			
 		} else if (publicacao instanceof Monografia) {
+			
 			TipoMonografia tipoMonografia = ((Monografia) publicacao).getTipoMonografia();
+			
 			if (tipoMonografia.equals(TipoMonografia.DISSERTACAO_MESTRADO)) {
-				Monografia dissertacaoMestrado = (Monografia) publicacao;
 				bib.append("@Mastersthesis{");
-				bib.append(getNomes(dissertacaoMestrado));
-				bib.append(",\n");
-				bib.append("author\t= \"");
-				bib.append(getNomeAutores(dissertacaoMestrado));
-				bib.append("\",\n");
-				bib.append("title\t= \"");
-				bib.append(dissertacaoMestrado.getTitulo());
-				bib.append("\",\n");
-				bib.append("school\t= \"");
-				bib.append(dissertacaoMestrado.getEscola());
-				bib.append("\",\n");
-				bib.append("year\t= \"");
-				bib.append(dissertacaoMestrado.getAno());
-				bib.append("\",\n");
-				bib.append("month\t= \"");
-				bib.append(dissertacaoMestrado.getMes());
-				bib.append("\",\n}");
 			} else if (tipoMonografia.equals(TipoMonografia.TESE_DOUTORADO)) {
-				Monografia teseDoutorado = (Monografia) publicacao;
 				bib.append("@Phdthesis{");
-				bib.append(getNomes(teseDoutorado));
-				bib.append(",\n");
-				bib.append("author\t= \"");
-				bib.append(getNomeAutores(teseDoutorado));
-				bib.append("\",\n");
-				bib.append("title\t= \"");
-				bib.append(teseDoutorado.getTitulo());
-				bib.append("\",\n");
-				bib.append("school\t= \"");
-				bib.append(teseDoutorado.getEscola());
-				bib.append("\",\n");
-				bib.append("year\t= \"");
-				bib.append(teseDoutorado.getAno());
-				bib.append("\",\n");
-				bib.append("month\t= \"");
-				bib.append(teseDoutorado.getMes());
-				bib.append("\",\n}");
 			}
+			
+			bib.append(nomesAutorTitulo + "school\t= \"" + ((Monografia) publicacao).getEscola() + "\",\n year\t= \"" + publicacao.getAno() + "\",\n month\t= \""
+					+ ((Monografia) publicacao).getMes() + "\",\n}");
 		}
+		
 		return bib;
 	}
 
