0af72437b893750523542297a787fdd2a3d134b7
fltiago <fltiago@gmail.com>
2010-11-08 09:47:07 -0200

    Modificando o velocity.

--- src/br/ufpe/cin/in980/util/Velocity.java
+++ src/br/ufpe/cin/in980/util/Velocity.java
@@ -3,6 +3,8 @@ package br.ufpe.cin.in980.util;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 
 import org.apache.velocity.app.VelocityEngine;
 import org.apache.velocity.exception.MethodInvocationException;
@@ -25,27 +27,35 @@ public class Velocity {
 			e.printStackTrace();
 		}
 		
-		ArrayList<String> list = new ArrayList<String>();
+		HashMap<String, String> map = new HashMap<String, String>();
 		
-		list.add("nome");
-		list.add("tipo");
-		list.add("tipoEstudante");
-		list.add("nomeorientador");
-		list.add("nomecoorientador");
-		list.add("departamento");
-		list.add("universidade");
-		list.add("email");
-		list.add("telefone");
-		list.add("website");
-		list.add("cidade");
-		list.add("pais");
-		list.add("status");
+		map.put("idmembro", "Long");
+		map.put("nome", "String");
+		map.put("tipo", "String");
+		map.put("tipoEstudante", "String");
+		map.put("nomeorientador", "String");
+		map.put("nomecoorientador", "String");
+		map.put("departamento", "String");
+		map.put("universidade", "String");
+		map.put("email", "String");
+		map.put("telefone", "String");
+		map.put("website", "String");
+		map.put("cidade", "String");
+		map.put("pais", "String");
+		map.put("foto", "byte[]");
+		map.put("status", "String");
 
-		
+		for (String string : map.keySet()) {
+			System.out.println(map.get(string));
+		}
         /*  next, get the Template  */
         /*  create a context and add data */
         VelocityContext context = new VelocityContext();
-        context.put("nameList", list);
+        context.put("attrs", map);
+        context.put("harParam", true);
+        context.put("$nomeClasse", "Membro");
+        context.put("nomeClasseMin", "membro");
+        
         /* now render the template into a StringWriter */
         StringWriter writer = new StringWriter();
         t.merge( context, writer );
